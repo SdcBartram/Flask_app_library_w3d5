8d8919659c10b60875b916956c8d9295d86a6420
Sam <sdcbartram@icloud.com>
2023-04-23 16:11:41 +0100

    update to formatting

--- models/books.py
+++ models/books.py
@@ -6,8 +6,10 @@ book3 = Book("Lamb to the Slaughter", "Roald Dahl", "Short Story", True)
 
 books = [book1, book2, book3]
 
+
 def add_new_book(book):
     books.append(book)
+
 
 def delete_book(book_title):
     book_to_delete = None
@@ -15,11 +17,10 @@ def delete_book(book_title):
         if book.title == book_title:
             book_to_delete = book
             break
-    
+
     books.remove(book_to_delete)
 
 # def check_out_book(book):
 #     for book in books:
 #         if book.title == book_title and not book.checked_out:
 #             book.checked_out = True
-            
