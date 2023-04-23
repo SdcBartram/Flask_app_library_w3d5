3fbfa453879c4bbc320e713a7d33356d31594f04
Sam <sdcbartram@icloud.com>
2023-04-23 15:55:16 +0100

    mvp and some extensions completed

---
+++ controllers/controller.py
@@ -0,0 +1,33 @@
+from app import app
+from flask import render_template, request, redirect
+from models.books import *
+from models.Book import *
+
+
+@app.route('/books')
+def index():
+    return render_template('index.jinja', title='Barts Books', books=books)
+
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    title = request.form['title']
+    author = request.form['author']
+    genre = request.form['genre']
+    # checked_out = True if 'checked_out' in request.form else False
+
+    new_book = Book(title, author, genre)
+    add_new_book(new_book)
+    return redirect('/books')
+
+
+@app.route('/books/delete/<title>', methods=['POST'])
+def delete(title):
+    delete_book(title)
+    return redirect('/books')
+
+
+@app.route('/books/<index>')
+def display_book(index):
+    book_to_display = books[int(index)]
+    return render_template('book.jinja', book=book_to_display)
---
+++ models/books.py
@@ -0,0 +1,25 @@
+from models.Book import *
+
+book1 = Book("When the Lion Feeds", "Wilbur Smith", "Adventure", True)
+book2 = Book("Pride and Prejudice", "Jane Austen", "Romance", False)
+book3 = Book("Lamb to the Slaughter", "Roald Dahl", "Short Story", True)
+
+books = [book1, book2, book3]
+
+def add_new_book(book):
+    books.append(book)
+
+def delete_book(book_title):
+    book_to_delete = None
+    for book in books:
+        if book.title == book_title:
+            book_to_delete = book
+            break
+    
+    books.remove(book_to_delete)
+
+# def check_out_book(book):
+#     for book in books:
+#         if book.title == book_title and not book.checked_out:
+#             book.checked_out = True
+            
